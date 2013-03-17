9a9d642450c3ef30020120cabad3ef607786f853
John Hogenmiller <john@hogenmiller.net>
2013-03-17 17:31:12 -0400

    added sitemap

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'John Hogenmiller'
+AUTHOR_EMAIL = u'john@hogenmiller.net'
 SITENAME = u'Pelican Test Site'
 SITEURL = 'http://pelican.yourtech.us'
 
@@ -9,8 +10,12 @@ TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
+PLUGINS = ['pelican.plugins.gravatar',]
+
 # Theme it
 THEME = "themes/pelican-bootstrap-responsive-theme"
+# THEME = "notmyidea"
+
 # Blogroll
 LINKS =  (
           ('id Graphics', 'http://www.idgraphics.net'),
@@ -24,3 +29,20 @@ SOCIAL = (('Myself', 'http://www.facebook.com/johnhogenmiller/'),
          )
 
 DEFAULT_PAGINATION = 10
+
+PLUGINS=['pelican.plugins.sitemap',]
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
